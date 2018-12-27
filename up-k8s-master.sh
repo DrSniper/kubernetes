32d0f63e4e3d91cc8f7bda1076166a0c89411f00
DrSniper <33818185+DrSniper@users.noreply.github.com>
2018-12-27 18:01:56 +0800

    Create up-k8s-master.sh

---
+++ up-k8s-master.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+read -p "是否添加node节点：1.yes   2.no:  " CHOICE
+if [ $CHOICE == 'yes' ];then
+	read -p "请输入需要添加的主机ip，用' '(空格)隔开： " NODE_IP
+	read -p  "请输入node节点的密码：" NODE_PASSWD
+fi
+echo "###1.系统准备"
+
+systemctl stop firewalld
+systemctl disable firewalld
+setenforce 0
+sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/sysconfig/selinux
+
+
+
+iptables -F
+iptables -t nat -F
+iptables -I FORWARD -s 0.0.0.0/0 -d 0.0.0.0/0 -j ACCEPT  
+
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward=1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-arptables = 1
+vm.swappiness=0
+EOF
+
+sysctl -p /etc/sysctl.d/k8s.conf
+
+swapoff -a 
+
+
+
+echo "####2.安装相关软件包"
+yum install -y yum-utils sshpass device-mapper-persistent-data lvm2
+
+echo "###3.安装docker"
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+yum makecache fast
+
+yum install -y --setopt=obsoletes=0 \
+  docker-ce-18.06.1.ce-3.el7
+
+systemctl start docker
+systemctl enable docker
+
+echo "###4.添加docker镜像仓库加速"
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "insecure-registries": ["`hostname -i`:5000"],
+  "registry-mirrors": ["https://ran9u71w.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+echo "###5.安装k8s"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl-1.12.1-0 kubelet-1.12.1-0 kubeadm-1.12.1-0
+systemctl enable kubelet && systemctl start kubelet
+
+echo "###6.准备k8s.grc.io镜像"
+bash yaml/pull-images.sh
+
+echo "###7.初始化master节点"
+kubeadm init \
+  --kubernetes-version=v1.12.1 \
+  --pod-network-cidr=10.254.0.0/16 \
+  --apiserver-advertise-address=`hostname -i` >kube-init.txt
+
+echo "###8.使master节点 ready"
+ mkdir -p $HOME/.kube
+ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+ sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "###9.将master节点同时设置为计算节点"
+kubectl taint nodes `hostname` node-role.kubernetes.io/master-
+
+echo "###10.添加网络插件[fannel]"
+kubectl apply -f  yaml/flannel.yaml
+
+echo "###11.安装dashboard"
+kubectl apply -f  yaml/k8s-dashboard.yaml
+
+echo "StrictHostKeyChecking no" >>/etc/ssh/ssh_config
+if [ $CHOICE == 1 ];then
+	 echo "###12.添加node节点"
+	 for i in $NODE_IP
+	 do
+	 	sshpass -p "$NODE_PASSWD" scp /root/up-k8s-node.sh root@$i:/root
+	 	sshpass -p "$NODE_PASSWD" scp /root/kube-init.txt root@$i:/root
+	 	sshpass -p "$NODE_PASSWD" ssh root@$i "bash /root/up-k8s-node.sh"
+	 done
+ 	sleep 200
+fi
+
+echo "###13.检查服务状态"
+kubectl get nodes
+kubectl get pods --all-namespaces
+
+echo "###14.访问"
+echo -e "请使用火狐浏览器访问dashboard界面：\n https://`hostname -i`:30001 "
+SECRET=`kubectl get secret -n kube-system|grep dashboard-token|awk '{print $1}'`
+TOKEN=`kubectl describe secret -n kube-system $SECRET|grep "token:"|awk '{print $2}'`
+echo -e "选择token方式访问，token为：\n $TOKEN"
