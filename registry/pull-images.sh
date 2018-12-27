2c8036744a382fc505d9894f9887a566a4b88dcc
DrSniper <33818185+DrSniper@users.noreply.github.com>
2018-12-27 16:15:39 +0800

    Update pull-images.sh

--- registry/pull-images.sh
+++ registry/pull-images.sh
@@ -2,7 +2,7 @@
 USERNAME=fingerliu
 LOCALIP=`hostname -i`
 docker run -d -p 5000:5000 --restart=always --name registry docker.io/registry
-images=(kube-apiserver:v1.12.1 kube-controller-manager:v1.12.1 kube-scheduler:v1.12.1 kube-proxy:v1.12.1 pause:3.1 etcd:3.2.24 coredns:1.2.2 kubernetes-dashboard-amd64:v1.10.0)
+images=(kube-apiserver:v1.12.2 kube-controller-manager:v1.12.2 kube-scheduler:v1.12.2 kube-proxy:v1.12.2 pause:3.1 etcd:3.2.24 coredns:1.2.2 kubernetes-dashboard-amd64:v1.10.0)
 for imageName in ${images[@]} ; do
   docker pull $USERNAME/$imageName
   docker tag $USERNAME/$imageName k8s.gcr.io/$imageName
