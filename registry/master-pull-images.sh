f8ea6112ecfc176cd95197e6fae1501b7cbd5dc6
DrSniper <33818185+DrSniper@users.noreply.github.com>
2018-12-27 18:09:50 +0800

    Update master-pull-images.sh

--- registry/master-pull-images.sh
+++ registry/master-pull-images.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 USERNAME=sniperwang
 LOCALIP=`hostname -i`
-docker run -d -p 5000:5000 --restart=always --name registry docker.io/registry
 images=(kube-apiserver:v1.12.2 kube-controller-manager:v1.12.2 kube-scheduler:v1.12.2 kube-proxy:v1.12.2 pause:3.1 etcd:3.2.24 coredns:1.2.2 kubernetes-dashboard-amd64:v1.10.0)
 for imageName in ${images[@]} ; do
   docker pull $USERNAME/$imageName
