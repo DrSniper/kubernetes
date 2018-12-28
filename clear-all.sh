20ad0b369b07456917bbd5978f6a87650cd8f849
DrSniper <33818185+DrSniper@users.noreply.github.com>
2018-12-28 10:41:57 +0800

    Create clear-all.sh

---
+++ clear-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+systemctl stop kubelet
+systemctl stop docker 
+
+yum remove -y kubelet kubeadm kubectl
+yum remove -y docker-ce
+
+rm -fr /root/.kube
+rm -fr /var/lib/kubelet
+rm -fr /etc/kubernetes/
+
+rm -fr /var/lib/docker 
+rm -fr /etc/docker 
