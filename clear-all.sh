45176dc6d0265cb286d957b74555f269fdeeb46d
DrSniper <33818185+DrSniper@users.noreply.github.com>
2018-12-28 10:45:03 +0800

    Update clear-all.sh

--- clear-all.sh
+++ clear-all.sh
@@ -6,7 +6,7 @@ systemctl stop docker
 yum remove -y kubelet kubeadm kubectl
 yum remove -y docker-ce
 
-rm -fr /root/.kube
+rm -fr /root/{.kube,.pki}
 rm -fr /var/lib/kubelet
 rm -fr /etc/kubernetes/
 
